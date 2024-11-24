2a1edc3d089f73893d9bdaba6d8517c96194290f
jooo0922 <jooo0922@gmail.com>
2024-11-25 00:43:36 +0900

    feat: load texture

--- src/main.cpp
+++ src/main.cpp
@@ -127,6 +127,29 @@ int main()
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
+  /** cube Texture 로드 */
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  int width, height, nrComponents;
+  unsigned char *data = stbi_load("resources/textures/wood.png", &width, &height, &nrComponents, 0);
+  if (data)
+  {
+    // glTexImage2D(GL_FRAMEBUFFER, 0, nrComponents, width, height, 0, nrComponents, GL_UNSIGNED_BYTE, data); // error version
+    glTexImage2D(GL_TEXTURE_2D, 0, nrComponents, width, height, 0, nrComponents, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  }
+  else
+  {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
   return 0;
 }
 
